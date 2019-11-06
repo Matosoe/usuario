083a7e5417fa7f45a3d18eb37c3edd346b1241c4
Matosoe <eduardo.matoso@gmail.com>
2019-11-05 21:03:43 -0300

    separada a API de perfil de usuario, primeiro commit da de usuario

---
+++ src/main/java/br/com/perfilcar/usuario/kafka/Producer.java
@@ -0,0 +1,20 @@
+package br.com.perfilcar.usuario.kafka;
+
+/*
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Producer {
+
+  private KafkaTemplate<String, String> producer;
+
+  public Producer(KafkaTemplate<String, String> producer) {
+    this.producer = producer;
+  }
+  public void send(String message) {
+    producer.send("topicoLog", message);
+  }
+}
+*/
